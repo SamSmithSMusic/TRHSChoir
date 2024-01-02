16b76b217de10fa6bc6ec48844cec130070d4c3e
Samuel Smith <samsmithsmusic@gmail.com>
2024-01-02 05:53:21 -0700

    Index based repertoire function finished

--- scripts/performances.js
+++ scripts/performances.js
@@ -1,4 +1,4 @@
-
+const brandsElement = document.querySelector('#brands');
 const carsElement = document.querySelector("#cars");
 let carList = [];
 let choir = document.title.split('|').map(item => item.trim()).pop();
@@ -11,32 +11,45 @@ const getCars = async () => {
     {
         displayCars(carList, carsElement);
     }
-    else{
+    else if (choir == "Repertoire")
+    {
+        displayBrands(carList, brandsElement);
+    } 
+    else
+    {
         displayCars(carList.filter(car => car.Choir.includes(choir)), carsElement);
     }
 }
 
-const reset = () => {
-    carsElement.innerHTML = '';
+const reset = (element) => {
+    element.innerHTML = '';
 } 
 
 const sortBy = (cars) => {
-    reset();
+
+    if (choir == "Repertoire")
+    {
+        reset(brandsElement);
+    }
+    else 
+    {
+        reset(carsElement);
+    }
+
     let filter = document.querySelector('#sortBy').value;
 
-    if (filter == "all" && choir != "Archive")
+    if (filter == "all" && choir != "Archive" && choir != "Repertoire")
     {
         displayCars(carList.filter(car => car.Choir.includes(choir)), carsElement);
     }
 
-    else if (choir != "Archive")
+    else if (choir != "Archive" && choir != "Repertoire" && filter != "all")
     {
         displayCars(cars.filter(car => car.Year.includes(filter) && car.Choir.includes(choir)), carsElement);
     }
 
-    else
+    else if (choir == "Archive")
     {
-        let filter = document.querySelector('#sortBy').value;
     switch (filter) {
         case "2022":
             displayCars(cars.filter(car => car.Year.includes('2022')), carsElement);
@@ -51,6 +64,16 @@ const sortBy = (cars) => {
             displayCars(cars, carsElement);
             break;
     }
+    }
+
+    else if (choir == "Repertoire" && filter == "all")
+    {
+        displayBrands(cars,brandsElement);
+    }
+
+    else if (choir == "Repertoire")
+    {
+        displayBrands(cars.filter(car => car.Year.includes(filter)),brandsElement);
     }
 }
 
@@ -75,6 +98,37 @@ const displayCars = (carList, carsElement) => {
         carsElement.appendChild(ar);
     });
 }
+
+const displayBrands = (carList, brandsElement) => {
+    const groupedData = {};
+
+    // Group the data by Choir and Year
+    carList.forEach(car => {
+        const key = `${car.Choir}-${car.Year}`;
+        if (!groupedData[key]) {
+            groupedData[key] = [];
+        }
+        groupedData[key].push(car);
+    });
+
+    // Loop through the grouped data and create articles for each Choir and Year combination
+    for (const key in groupedData) {
+        const choirYearData = groupedData[key];
+
+        let choirYearArticle = document.createElement('article');
+        let h3 = document.createElement('h3');
+        h3.innerHTML = choirYearData[0].Choir + ' ' + choirYearData[0].Year;
+        choirYearArticle.appendChild(h3);
+
+        let songsParagraph = document.createElement('p');
+        choirYearData.forEach(car => {
+            songsParagraph.innerHTML += `${car.Song}<br>`;
+        });
+
+        choirYearArticle.appendChild(songsParagraph);
+        brandsElement.appendChild(choirYearArticle);
+    }
+};
 
 getCars();
 
