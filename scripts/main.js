e94257307e92ce2816b5dc1e0990e1e5d17c21f6
Samuel Smith <samsmithsmusic@gmail.com>
2024-03-31 12:06:05 -0600

    dropdownfix

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,4 @@ document.querySelector('#year').textContent = new Date().getFullYear();
 const toggleMenu = () => {
     document.querySelector('#menu').classList.toggle('open');
 }
+document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
