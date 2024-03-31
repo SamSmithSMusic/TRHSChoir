de9d551091223c2873c012e424a00228006beb23
Samuel Smith <SamSmithSmusic@gmail.com>
2024-03-31 00:23:12 -0600

    added calendar and agenda for mobile

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,3 @@ document.querySelector('#year').textContent = new Date().getFullYear();
 const toggleMenu = () => {
     document.querySelector('#menu').classList.toggle('open');
 }
-
-document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
